ae6c8700308378acf54957c84d41a7a9a65e450d
pknguyen2704 <pknguyen2704@gmail.com>
2025-04-10 21:39:58 +0700

    make controller_layer

--- src/controllers/boardController.js
+++ src/controllers/boardController.js
@@ -1,5 +1,17 @@
-/**
- * Updated by trungquandev.com's author on August 17 2023
- * YouTube: https://youtube.com/@trungquandev
- * "A bit of fragrance clings to the hand that gives flowers!"
- */
+import { StatusCodes } from 'http-status-codes'
+
+const createNew = async (req, res, next) => {
+
+  try {
+    console.log('request body final', req.body)
+    res.status(StatusCodes.CREATED).json({ message:'ControllerLayer: API create new board' })
+  } catch (error) {1
+    res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({
+      errors: error.message
+    })
+  }
+}
+
+export const boardController = {
+  createNew
+}
