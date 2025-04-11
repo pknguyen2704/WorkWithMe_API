87309e4481a8e992c37f2956e11f44dac1421faa
pknguyen2704 <pknguyen2704@gmail.com>
2025-04-11 23:12:40 +0700

    getBoardAPI

--- src/validations/boardValidation.js
+++ src/validations/boardValidation.js
@@ -1,6 +1,7 @@
 import { StatusCodes } from 'http-status-codes'
 import Joi from 'joi'
 import ApiError from '~/utils/ApiError'
+
 const createNew = async (req, res, next) => {
 
   const correctCondition = Joi.object({
@@ -13,9 +14,7 @@ const createNew = async (req, res, next) => {
     next()
 
   } catch (error) {
-    const errorMessage = new Error(error).message
-    const customError = new ApiError(StatusCodes.UNPROCESSABLE_ENTITY, errorMessage)
-    next(customError)
+    next(new ApiError(StatusCodes.UNPROCESSABLE_ENTITY, new Error(error).message))
   }
 }
 
